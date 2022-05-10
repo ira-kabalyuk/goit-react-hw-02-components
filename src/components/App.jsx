e2ba9f3554b16671088eeb7b270bc91d52988fdd
Irina Kabaliuk <i.kabaliuk@weplay.tv>
2022-05-10 20:47:13 +0300

    hw-2 feedback fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,58 @@
-import { Container } from "./Container";
-import { Feedback } from "./Feedback";
+import { Component } from 'react';
+import { Container } from './Container';
+import { Statistic } from './Statistic';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Notification } from './Notification';
 
-export const App = () => {
-  return (
-    <Container>
-      <Feedback />
-    </Container>
-  );
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleLeaveFeedback = type => {
+    this.setState(prevState => {
+      return {
+        [type]: prevState[type] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  positivePercentage = () => {
+    const totalValue = this.countTotalFeedback();
+    return Math.round((this.state.good * 100) / totalValue);
+  };
+
+  render() {
+    const options = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <Container>
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={this.handleLeaveFeedback}
+        />
+        {this.countTotalFeedback() >= 1 ? (
+          <Statistic
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.positivePercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Container>
+    );
+  }
+}
+
+export { App };
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,21 +1,19 @@
 import PropTypes from 'prop-types';
-import styles from './Section.module.scss'
+import styles from './Section.module.scss';
 
 const Section = ({ title, children, className }) => {
   return (
     <div className={className}>
-      {title && (
-        <h1 className={styles.title}>{title}</h1>
-      )}
+      {title && <h1 className={styles.title}>{title}</h1>}
       {children}
     </div>
-  )
-}
-  
+  );
+};
+
 Section.propTypes = {
   title: PropTypes.string,
   className: PropTypes.string,
   children: PropTypes.node.isRequired,
-}
+};
 
-export {Section}
+export { Section };
